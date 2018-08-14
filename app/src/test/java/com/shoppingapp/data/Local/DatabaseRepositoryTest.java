086b49c10bb50190b8e378983d84f5662e62423f
rajat028 <rajatarora2812@gmail.com>
2018-08-14 19:26:20 +0530

    DatabaseRepositoryTest Implemented.

---
+++ app/src/test/java/com/shoppingapp/data/Local/DatabaseRepositoryTest.java
@@ -0,0 +1,72 @@
+package com.shoppingapp.data.Local;
+
+import com.shoppingapp.data.model.OrderModel;
+import com.shoppingapp.data.model.Products;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class DatabaseRepositoryTest {
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private
+    ProductDAO productDAO;
+
+    private DatabaseRepository databaseRepository;
+
+    private Products.ProductsBean productsBean = new Products.ProductsBean();
+    private OrderModel orderModel = new OrderModel();
+
+    @Before
+    public void setUp() {
+        databaseRepository = new DatabaseRepository(productDAO);
+    }
+
+    @Test
+    public void shouldReturnGetAllProductsIsCalled() {
+        databaseRepository.getAllProducts();
+    }
+
+    @Test
+    public void shouldReturnGetCartProductsIsCalled() {
+        databaseRepository.getCartProducts();
+    }
+
+    @Test
+    public void shouldReturnGetAllOrdersIsCalled() {
+        databaseRepository.getAllOrder();
+    }
+
+    @Test
+    public void shouldReturnGetOrderByProductIdIsCalled() {
+        databaseRepository.getProductByOrderId("1");
+    }
+
+    @Test
+    public void shouldReturnInsertProductIsCalled() {
+        databaseRepository.insertProduct(productsBean);
+    }
+
+    @Test
+    public void shouldReturnUpdateProductIsCalled() {
+        databaseRepository.updateProduct(productsBean);
+    }
+
+    @Test
+    public void shouldReturnUpdateAllProductsIsCalled() {
+        databaseRepository.updateAllProducts(false, "One Plus 6T");
+    }
+
+    @Test
+    public void shouldReturnInsertOrderIsCalled() {
+        databaseRepository.insertOrder(orderModel);
+    }
+
+}
